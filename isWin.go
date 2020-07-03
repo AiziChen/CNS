d399325876c62ae2a7a2fef1601d085cc17b0eb0
aizi <1098679804@qq.com>
2020-07-03 16:38:38 +0800

    bug fix

--- isWin.go
+++ isWin.go
@@ -15,7 +15,7 @@ func setMaxNofile() {
 func setsid() {
 }
 
-func enableTcpFastopen(listener *net.TCPListener) {
+func enableTcpFastOpen(listener *net.TCPListener) {
 	const CNS_TCP_FASTOPEN int = 0x17
 	f, _ := listener.File()
 	if err := syscall.SetsockoptInt(syscall.Handle(f.Fd()), syscall.IPPROTO_TCP, CNS_TCP_FASTOPEN, 1); err != nil {
